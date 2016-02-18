81c756f2e62e33fac05982b154033c0a20c745ac
borriquear <borriquear@gmail.com>
2016-02-18 16:55:57 -0500

    Delete savesettomat.m
    obsolete, replaced by cuteoneepoch

--- savesettomat.m
+++
@@ -1,9 +0,0 @@
-% convert .set into .mat
-
- mydir = 'C:\Users\shelagh\Desktop\patients\Cordeiro, Jessica\Session 1 day 1 20 October\dp_cj28_20151020_s1'
- myfile = 'EEG_raw_cj28_20151020_s1.mat'
- myfullname = fullfile(mydir, myfile)
- save(myfullname, 'EEG')
- timelimits2 = [52799 5526122]
- EEGcutbsnormal =  eeg_eegrej(EEGcutbsnormal1, timelimits2);
- save('cut1_bs_induction_filteredbp-notch_cj28_20151020_s1.mat','EEGcutbsinduction')
